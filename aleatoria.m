90c58cc6e291f2c3d33dce07175223aeb6914833
Thiago Miranda <thiagocarvalho92@gmail.com>
2018-02-22 11:54:28 -0300

    nova versão

--- aleatoria.m
+++ aleatoria.m
@@ -1,8 +1,21 @@
 function y = aleatoria(T_min, T_max, A_min, A_max, offset)
 
-T_now = T_min + (T_max - T_min)*rand;
-A_now = A_min + (A_max - A_min)*rand + offset;
+persistent T_now;
+persistent A_now;
+persistent counter_a;
 
-pause(T_now)
+if isempty(counter_a)
+    T_now = T_min + (T_max - T_min)*rand;
+    A_now = A_min + (A_max - A_min)*rand + offset;
+    counter_a = 0;
+end
+
+counter_a = counter_a + 1;
 
 y = A_now;
+
+if (counter_a >= T_now*10)
+    T_now = T_min + (T_max - T_min)*rand;
+    A_now = A_min + (A_max - A_min)*rand + offset;
+    counter_a = 0;
+end
