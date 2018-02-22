8b3d9de04eb226805993cb119e274abecc18ca32
Thiago Miranda <thiagocarvalho92@gmail.com>
2018-02-22 11:53:53 -0300

    nova versão

--- dente.m
+++ dente.m
@@ -1,3 +1,16 @@
-function y = dente(t, T, offset, A)
+function y = dente(T, offset, A)
 
-y = A*sawtooth(t*2*pi/T) + offset;
+persistent counter_d;
+
+if isempty(counter_d)
+   counter_d = 0; 
+end
+
+
+y = counter_d*A/(10*T) + offset;
+
+counter_d = counter_d + 1;
+
+if (counter_d > T*10)
+   counter_d = 0;
+end
