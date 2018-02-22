ae769fedf2287c5689e99718c4bc6b01b016234f
Thiago Miranda <thiagocarvalho92@gmail.com>
2018-02-22 11:54:57 -0300

    nova versão

--- mux.m
+++ mux.m
@@ -2,13 +2,15 @@ function y = mux(degrau, seno, quadrada, dente, aleatoria, sel)
 
 switch sel
     case 1
-        y = seno;
-    case 2
         y = degrau;
+    case 2
+        y = seno;
     case 3
-        y = dente;
-    case 4
         y = quadrada;
+    case 4
+        y = dente;
     case 5
         y = aleatoria;
+    otherwise
+        y = 0;
 end
