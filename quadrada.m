61ffbb49aeea4740a5089a96e68a6e8b2f82dca5
Thiago Miranda <thiagocarvalho92@gmail.com>
2018-02-22 11:54:11 -0300

    nova versão

--- quadrada.m
+++ quadrada.m
@@ -1,3 +1,21 @@
-function y = quadrada(t, T, offset, A)
+function y = quadrada(T, offset, A)
 
-y = A*square(t*2*pi/T) + offset;
+persistent counter_q;
+
+if isempty(counter_q)
+   counter_q = 0; 
+end
+
+counter_q = counter_q + 1;
+
+if (counter_q <= 5*T)
+    y = offset;
+elseif (counter_q > 5*T)
+    y = A + offset;
+else
+    y = .5;
+end
+
+if (counter_q >= 10*T)
+   counter_q = 0; 
+end
