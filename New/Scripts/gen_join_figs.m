2f9936c95e7847bb9a44d5855283f513217b1775
Matheus Tenorio dos Santos <tenoriomatheus@ic.ufal.br>
2018-04-05 14:57:45 -0300

    Adicionando as imagens dos plots
    Signed-off-by: Matheus Tenorio dos Santos <tenoriomatheus@ic.ufal.br>

--- New/Scripts/gen_join_figs.m
+++ New/Scripts/gen_join_figs.m
@@ -37,3 +37,32 @@ join_figs('../Plots/Fig/Tanque2/Valores_Icaro/', '../Plots/Img/Tanque2/Valores_I
 join_figs('../Plots/Fig/Tanque2/Valores_Icaro/', '../Plots/Img/Tanque2/Valores_Icaro/', 'ctr_pi-d_val_icaro', '_pid2', '_tank2')
 join_figs('../Plots/Fig/Tanque2/Valores_Icaro/', '../Plots/Img/Tanque2/Valores_Icaro/', 'ctr_pid_val_icaro', '_pid2', '_tank2')
 
+%% Cascata
+join_figs('../Plots/Fig/Cascata/', '../Plots/Img/Cascata/', 'pid_mestre', '_pid', '_tanque')
+join_figs('../Plots/Fig/Cascata/', '../Plots/Img/Cascata/', 'pid_escravo', '_pid', '_tanque')
+
+%% Mudando Ref
+%% Tanque1
+%% Valores_Estimados
+join_figs('../Plots/Fig/Tanque1/Valores_Estimados/Mudando_Ref/', '../Plots/Img/Tanque1/Valores_Estimados/Mudando_Ref/', 'ctr_i-pd_nosso', '_pid', '_tanque')
+join_figs('../Plots/Fig/Tanque1/Valores_Estimados/Mudando_Ref/', '../Plots/Img/Tanque1/Valores_Estimados/Mudando_Ref/', 'ctr_pi-d_nosso', '_pid', '_tanque')
+join_figs('../Plots/Fig/Tanque1/Valores_Estimados/Mudando_Ref/', '../Plots/Img/Tanque1/Valores_Estimados/Mudando_Ref/', 'ctr_pid_nosso', '_pid', '_tanque')
+
+%% Valores_Icaro
+join_figs('../Plots/Fig/Tanque1/Valores_Icaro/Mudando_Ref/', '../Plots/Img/Tanque1/Valores_Icaro/Mudando_Ref/', 'ctr_i-pd_val_icaro', '_pid', '_tanque')
+join_figs('../Plots/Fig/Tanque1/Valores_Icaro/Mudando_Ref/', '../Plots/Img/Tanque1/Valores_Icaro/Mudando_Ref/', 'ctr_pi-d_val_icaro', '_pid', '_tanque')
+join_figs('../Plots/Fig/Tanque1/Valores_Icaro/Mudando_Ref/', '../Plots/Img/Tanque1/Valores_Icaro/Mudando_Ref/', 'ctr_pid_val_icaro', '_pid', '_tanque')
+
+%% Valores_ZN
+join_figs('../Plots/Fig/Tanque1/Valores_ZN/Mudando_Ref/', '../Plots/Img/Tanque1/Valores_ZN/Mudando_Ref/', 'ctr_pid_val_zn', '_pid', '_tanque')
+
+%% Tanque2
+%% Valores_Estimados
+join_figs('../Plots/Fig/Tanque2/Valores_Estimados/Mudando_Ref/', '../Plots/Img/Tanque2/Valores_Estimados/Mudando_Ref/', 'ctr_i-pd_val_nosso', '_pid2', '_tanque2')
+join_figs('../Plots/Fig/Tanque2/Valores_Estimados/Mudando_Ref/', '../Plots/Img/Tanque2/Valores_Estimados/Mudando_Ref/', 'ctr_pi-d_val_nosso', '_pid2', '_tanque2')
+join_figs('../Plots/Fig/Tanque2/Valores_Estimados/Mudando_Ref/', '../Plots/Img/Tanque2/Valores_Estimados/Mudando_Ref/', 'ctr_pid_val_nosso', '_pid2', '_tanque2')
+
+%% Valores_Icaro
+join_figs('../Plots/Fig/Tanque2/Valores_Icaro/Mudando_Ref/', '../Plots/Img/Tanque2/Valores_Icaro/Mudando_Ref/', 'ctr_i-pd_val_icaro', '_pid2', '_tanque2')
+join_figs('../Plots/Fig/Tanque2/Valores_Icaro/Mudando_Ref/', '../Plots/Img/Tanque2/Valores_Icaro/Mudando_Ref/', 'ctr_pi-d_val_icaro', '_pid2', '_tanque2')
+join_figs('../Plots/Fig/Tanque2/Valores_Icaro/Mudando_Ref/', '../Plots/Img/Tanque2/Valores_Icaro/Mudando_Ref/', 'ctr_pid_val_icaro', '_pid2', '_tanque2')
